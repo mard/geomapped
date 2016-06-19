4a3b4e3d44dcfd6c71c2fbacb801c1e34afff307
Mariusz Dziemianowicz <mariusz@dziemianowi.cz>
2016-06-20 00:13:26 +0200

    add library download script

---
+++ build.py
@@ -0,0 +1,36 @@
+import os
+import urllib.request
+
+def create_dir(path):
+  if not os.path.isdir(path):
+    print('Creating directory: %s' % path)
+    os.makedirs(path)
+
+def download_file(source, target):
+  target = os.path.abspath(os.path.expanduser(target))
+  create_dir(os.path.dirname(target))
+  print('Downloading: %s' % source)
+  remote = urllib.request.urlopen(source)
+  with open(target,'wb') as output:
+    output.write(remote.read())
+
+# jqueery
+download_file('https://code.jquery.com/jquery-3.0.0.min.js', './public_html/lib/jquery/jquery.min.js')
+
+# leaflet
+download_file('http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js', './public_html/lib/leaflet/leaflet.js')
+download_file('http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.css', './public_html/lib/leaflet/leaflet.css')
+download_file('http://cdn.leafletjs.com/leaflet/v0.7.7/images/layers.png', './public_html/lib/leaflet/images/layers.png')
+download_file('http://cdn.leafletjs.com/leaflet/v0.7.7/images/layers-2x.png', './public_html/lib/leaflet/images/layers-2x.png')
+download_file('http://cdn.leafletjs.com/leaflet/v0.7.7/images/marker-icon.png', './public_html/lib/leaflet/images/marker-icon.png')
+download_file('http://cdn.leafletjs.com/leaflet/v0.7.7/images/marker-icon-2x.png', './public_html/lib/leaflet/images/marker-icon-2x.png')
+download_file('http://cdn.leafletjs.com/leaflet/v0.7.7/images/marker-shadow.png', './public_html/lib/leaflet/images/marker-shadow.png')
+
+# leaflet-gpx
+download_file('https://raw.githubusercontent.com/mpetazzoni/leaflet-gpx/master/gpx.js', './public_html/lib/leaflet-gpx/gpx.js')
+download_file('https://raw.githubusercontent.com/mpetazzoni/leaflet-gpx/master/pin-icon-end.png', './public_html/lib/leaflet-gpx/pin-icon-end.png')
+download_file('https://raw.githubusercontent.com/mpetazzoni/leaflet-gpx/master/pin-icon-start.png', './public_html/lib/leaflet-gpx/pin-icon-start.png')
+download_file('https://raw.githubusercontent.com/mpetazzoni/leaflet-gpx/master/pin-shadow.png', './public_html/lib/leaflet-gpx/pin-shadow.png')
+
+# leaflet multi options polyline
+download_file('https://raw.githubusercontent.com/hgoebl/Leaflet.MultiOptionsPolyline/master/Leaflet.MultiOptionsPolyline.min.js', './public_html/lib/leaflet-multioptionspolyline/Leaflet.MultiOptionsPolyline.min.js');
